a2642bb34042c09b47383a9ec4731c3e7a06dc72
s.masoumbeigi <sbeygi712@gmail.com>
2024-02-17 15:02:35 +0330

    adding responsive extension

--- packages/utility/lib/src/extensions/widget_extensions.dart
+++ packages/utility/lib/src/extensions/widget_extensions.dart
@@ -7,6 +7,27 @@ extension ContextExtension on BuildContext {
   double get height => MediaQuery.of(this).size.height;
 }
 
+extension Responsive on BuildContext {
+  T responsive<T>(
+      T defaultVal, {
+        T? sm,
+        T? md,
+        T? lg,
+        T? xl,
+      }) {
+    final wd = MediaQuery.of(this).size.width;
+    return wd >= 1280
+        ? (xl ?? lg ?? md ?? sm ?? defaultVal)
+        : wd >= 1024
+        ? (lg ?? md ?? sm ?? defaultVal)
+        : wd >= 768
+        ? (md ?? sm ?? defaultVal)
+        : wd >= 640
+        ? (sm ?? defaultVal)
+        : defaultVal;
+  }
+}
+
 extension StringExtension on String {
   Widget toLabel({TextStyle? textStyle, TextAlign? textAlign}) => Label(this,textStyle: textStyle, textAlign: textAlign);
   Widget imageWidget({Color? color,double? width,double? height,BoxFit? boxFit}) => ImageWidget(path: this,color: color,width: width,height: height, boxFit: boxFit);
