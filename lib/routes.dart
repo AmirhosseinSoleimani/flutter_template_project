3f5714be4982296549491af508c7de594d43dd34
Soli Hosseini <s.hosseini.309@gmail.com>
2024-02-17 13:51:08 +0330

    #modified files

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,56 +1,77 @@
-import 'package:eks_khedamtresan/features/authentication/presentation/screens/auth_screen.dart';
-import 'package:eks_khedamtresan/features/home/presentation/screens/home_screen.dart';
-import 'package:eks_khedamtresan/features/main/presentation/screens/main_screen.dart';
-import 'package:eks_khedamtresan/features/profile/presentation/screens/profile_screen.dart';
-import 'package:eks_khedamtresan/features/splash/presentation/screens/splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'features/features.dart';
 
+
+/// A class that contains all the routes for the application.
 class Routes {
-  static final GlobalKey<NavigatorState> parentNavigatorKey =
-  GlobalKey<NavigatorState>();
-  static final GlobalKey<NavigatorState> homeTabNavigatorKey =
-  GlobalKey<NavigatorState>();
-  static final GlobalKey<NavigatorState> profileTabNavigatorKey =
-  GlobalKey<NavigatorState>();
+  /// A global key for the parent navigator.
+  static final parentNavigatorKey = GlobalKey<NavigatorState>();
 
+  /// A global key for the home tab navigator.
+  static final homeTabNavigatorKey = GlobalKey<NavigatorState>();
+
+  /// A global key for the profile tab navigator.
+  static final profileTabNavigatorKey = GlobalKey<NavigatorState>();
+
+  /// The routes for the application.
   static final routes = GoRouter(
+    /// The key for the navigator used by the routes.
     navigatorKey: parentNavigatorKey,
     routes: [
       GoRoute(
+        /// The path for the splash screen.
         path: '/',
+        /// The builder for the splash screen.
         builder: (context, state) => const SplashScreen(),
       ),
       GoRoute(
+        /// The path for the authentication screen.
         path: '/auth',
+        /// The builder for the authentication screen.
         builder: (context, state) => const AuthScreen(),
       ),
-
+      /// A route that uses an indexed stack with two branches.
       StatefulShellRoute.indexedStack(
+        /// The key for the navigator used by the branches.
         parentNavigatorKey: parentNavigatorKey,
         branches: [
+          /// The first branch of the indexed stack.
           StatefulShellBranch(
+            /// The key for the navigator used by the routes in the branch.
             navigatorKey: homeTabNavigatorKey,
+            /// The routes for the first branch.
             routes: [
               GoRoute(
+                /// The path for the main screen in the first branch.
                 path: '/main',
-                pageBuilder: (context, GoRouterState state) {
+                /// The builder for the main screen in the first branch.
+                pageBuilder: (context, state) {
                   return getPage(
+                    /// The child for the main screen.
                     child: const HomeScreen(),
+                    /// The state for the current route.
                     state: state,
                   );
                 },
               ),
             ],
           ),
+          /// The second branch of the indexed stack.
           StatefulShellBranch(
+            /// The key for the navigator used by the routes in the branch.
             navigatorKey: profileTabNavigatorKey,
+            /// The routes for the second branch.
             routes: [
               GoRoute(
+                /// The path for the profile screen in the second branch.
                 path: '/profile',
-                pageBuilder: (context, GoRouterState state) {
+                /// The builder for the profile screen in the second branch.
+                pageBuilder: (context, state) {
                   return getPage(
+                    /// The child for the profile screen.
                     child: const ProfileScreen(),
+                    /// The state for the current route.
                     state: state,
                   );
                 },
@@ -58,30 +79,36 @@ class Routes {
             ],
           ),
         ],
+        /// The builder for the main screen.
         pageBuilder: (
-            BuildContext context,
-            GoRouterState state,
-            StatefulNavigationShell navigationShell,
-            ) {
+          context,
+          state,
+          navigationShell,
+        ) {
           return getPage(
+            /// The child for the main screen.
             child: MainScreen(
+              /// The child for the navigation shell.
               child: navigationShell,
             ),
+            /// The state for the current route.
             state: state,
           );
         },
       ),
-
     ],
   );
-}
 
-Page getPage({
-  required Widget child,
-  required GoRouterState state,
-}) {
-  return MaterialPage(
-    key: state.pageKey,
-    child: child,
-  );
+  /// Returns a page for the given child and state.
+  static Page getPage({
+    required Widget child,
+    required GoRouterState state,
+  }) {
+    return MaterialPage(
+      /// The key for the page.
+      key: state.pageKey,
+      /// The child for the page.
+      child: child,
+    );
+  }
 }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,10 +5,10 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+import 'package:eks_khedamtresan/app.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:eks_khedamtresan/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
